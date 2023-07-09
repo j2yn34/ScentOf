379174232c3130c2f66208d1d87afa45ecb2f166
Jiyeon Park <wldusys34@gmail.com>
2023-07-09 19:09:58 +0900

    Design: 메인페이지 타이틀 소제목 추가, 더보기 버튼 및 리뷰 컴포넌트 스타일 수정
    - 더보기 버튼 아이콘 및 호버 스타일 추가
    - 리뷰글 텍스트 컬러 수정

--- src/components/posts/ReviewPost.tsx
+++ src/components/posts/ReviewPost.tsx
@@ -24,13 +24,13 @@ const ReviewPost = (): JSX.Element => {
             <Rating rating={review.rating} />
           </div>
           <div className="card-body p-0 pl-4 gap-0">
-            <span className="text-xs text-brown-300">{review.brandName}</span>
-            <h2 className="card-title py-1 text-brown-600">
+            <span className="text-sm text-brown-300">{review.brandName}</span>
+            <h2 className="card-title py-1 text-brown-900">
               {review.productName}
             </h2>
-            <p className="text-sm text-brown-400">{review.reviewTitle}</p>
+            <p className="text-sm text-brown-500">{review.reviewTitle}</p>
             <div className="flex flex-col items-end mt-2">
-              <span className="text-right text-sm text-brown-500 my-1">
+              <span className="text-right text-sm text-brown-400 my-1">
                 {review.nickname}
               </span>
             </div>
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,26 +6,34 @@ const Index = () => {
   return (
     <div>
       <div className="pt-14 px-4">
-        <h2 className="mb-5 lg:mb-8 text-2xl lg:text-2xl text-left font-bold">
-          향기 리뷰
-        </h2>
-        <ReviewPost />
-        <div className="mt-8 text-center">
-          <Link to="/review">
-            <span className="underline underline-offset-4">더보기</span>
-          </Link>
+        <div className="flex items-end mb-5 lg:mb-8 ">
+          <h2 className="text-2xl lg:text-2xl text-left font-bold">
+            향기 리뷰
+          </h2>
+          <span className="ml-3.5 text-brown-400">업데이트 된 리뷰</span>
         </div>
+        <ReviewPost />
+        <Link to="/review" className="block w-[70px] mx-auto">
+          <div className="mt-8 flex justify-center items-center hover:border-b hover:border-brown-500 transition-all">
+            <span className="text-brown-500">더보기</span>
+            <span className="ml-1 block aria-hidden w-[8px] h-[8px] border-t border-r border-brown-500 origin-center rotate-45"></span>
+          </div>
+        </Link>
       </div>
       <div className="pt-14 px-4">
-        <h2 className="mb-5 lg:mb-8 text-2xl lg:text-2xl text-left font-bold">
-          제품 추천
-        </h2>
-        <RecommendPost />
-        <div className="mt-8 text-center">
-          <Link to="/recommend">
-            <span className="underline underline-offset-4">더보기</span>
-          </Link>
+        <div className="flex items-end mb-5 lg:mb-8 ">
+          <h2 className="text-2xl lg:text-2xl text-left font-bold">
+            추천 질문
+          </h2>
+          <span className="ml-3.5 text-brown-400">추천해 주세요!</span>
         </div>
+        <RecommendPost />
+        <Link to="/recommend" className="block w-[70px] mx-auto">
+          <div className="mt-8 flex justify-center items-center hover:border-b hover:border-brown-500 transition-all">
+            <span className="text-brown-500">더보기</span>
+            <span className="ml-1 block aria-hidden w-[8px] h-[8px] border-t border-r border-brown-500 origin-center rotate-45"></span>
+          </div>
+        </Link>
       </div>
     </div>
   );
