808f1a0d878944c1cf91ed229d28212b3d6e9732
Jiyeon Park <wldusys34@gmail.com>
2023-07-11 04:19:24 +0900

    Feat: Recommend 페이지 및 Pagination 컴포넌트 구현
    - 추천 글 목록 전체 페이지 구현
    - 페이지네이션 컴포넌트 구현
    - RecommendPost 코드 리팩토링
    
    To Do
    - 데이터 불러오는 limit 개수를 부모 컴포넌트에서 지정할 수 있도록 수정해야 함.
    - 꺽쇠 모양 className이 너무 길어서 가독성 떨어짐. 재사용 가능하도록 구현 후 코드 리팩토링 예정

---
+++ src/components/common/Pagination.tsx
@@ -0,0 +1,65 @@
+type Pagenation = {
+  currentPage: number;
+  maxPage: number;
+  onClickPageButton: (pageNumber: number) => void;
+};
+
+type PageButton = {
+  pageNumber: number;
+  onClick: () => void;
+  selected: boolean;
+};
+
+const Pagination = ({
+  currentPage,
+  maxPage,
+  onClickPageButton,
+}: Pagenation): JSX.Element => {
+  return (
+    <div className="mt-9 flex justify-center items-center">
+      <button
+        className={`flex flex-row items-center ${
+          currentPage === 1 ? "disabled" : ""
+        }`}
+      >
+        <span className="block aria-hidden w-[8px] h-[8px] border-b border-l border-brown-400 origin-center rotate-45" />
+        <span className="text-brown-400 mr-2.5 text-sm">이전</span>
+      </button>
+      {new Array(maxPage).fill(null).map((_, index) => (
+        <PageButton
+          key={index}
+          pageNumber={index + 1}
+          onClick={() => onClickPageButton(index + 1)}
+          selected={index + 1 === currentPage}
+        />
+      ))}
+      <button
+        className={`flex flex-row items-center ${
+          currentPage === maxPage ? "disabled" : ""
+        }`}
+      >
+        <span className="text-brown-400 ml-2.5 text-sm">다음</span>
+        <span className="block aria-hidden w-[8px] h-[8px] border-t border-r border-brown-400 origin-center rotate-45" />
+      </button>
+    </div>
+  );
+};
+
+const PageButton = ({
+  pageNumber,
+  onClick,
+  selected,
+}: PageButton): JSX.Element => {
+  return (
+    <button
+      className={`text-brown-500 px-3 py-1 hover:underline rounded ${
+        selected ? "selected" : ""
+      }`}
+      onClick={onClick}
+    >
+      {pageNumber}
+    </button>
+  );
+};
+
+export default Pagination;
--- src/components/posts/RecommendPost.tsx
+++ src/components/posts/RecommendPost.tsx
@@ -5,20 +5,20 @@ const RecommendPost = (): JSX.Element => {
   const limit = 4;
 
   return (
-    <div>
-      <div className="flex flex-col">
-        {recommendData.slice(0, limit).map((recommend, index) => (
-          <Link to={`/recommend/${recommend.id}`} key={index}>
-            <div className="flex items-center justify-between bg-beige w-full min-h-[80px] p-4 border-t border-brown-200 ">
-              <p className="text-brown-900 text-md">{recommend.title}</p>
-              <div className="text-brown-400 md:text-sm sm:text-xs">
-                <span className="px-4">{recommend.nickname}</span>
-                <span>2023-7-9</span>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
+    <div className="flex flex-col">
+      {recommendData.slice(0, limit).map((recommend, index) => (
+        <Link
+          to={`/recommend/${recommend.id}`}
+          key={index}
+          className="flex items-center justify-between bg-beige w-full min-h-[64px] p-4 border-t border-brown-200 "
+        >
+          <p className="text-brown-900 text-md">{recommend.title}</p>
+          <div className="text-brown-400 md:text-sm sm:text-xs">
+            <span className="px-4">{recommend.nickname}</span>
+            <span>2023-07-09</span>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
