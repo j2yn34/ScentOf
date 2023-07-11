23be6a662a331361749e66611b362d69e4572616
Jiyeon Park <wldusys34@gmail.com>
2023-07-11 21:07:34 +0900

    Add: 로그인 및 회원가입 페이지 레이아웃
    ToDo
    - 스타일 및 유효성 검사 메시지 수정 필요

--- src/pages/member/Login.tsx
+++ src/pages/member/Login.tsx
@@ -1,5 +1,62 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return <>Login</>;
+  return (
+    <div>
+      <div
+        className="flex items-center justify-center mx-auto text-center"
+        style={{ minHeight: `calc(100vh - 64px - 4rem - 81px)` }}
+      >
+        <div className="flex flex-col items-center max-w-md w-full">
+          <h1 className="text-2xl text-brown-900 mb-4">로그인</h1>
+          <div className="form-control w-full max-w-xs">
+            <input
+              type="email"
+              placeholder="이메일을 입력해 주세요."
+              className="input w-full placeholder:text-sm"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+            </label>
+            <input
+              type="password"
+              placeholder="비밀번호를 입력해 주세요."
+              className="input w-full placeholder:text-sm"
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+            </label>
+
+            <button
+              className="btn bg-brown-500 text-white hover:bg-brown-600 my-2.5"
+              type="submit"
+            >
+              로그인
+            </button>
+            <button
+              className="btn bg-white text-brown hover:bg-beige"
+              type="submit"
+            >
+              구글 로그인
+            </button>
+          </div>
+
+          <div className="mt-8 w-full max-w-xs">
+            <p className="mb-3 text-sm">
+              아직 <b>센트오브</b>의 회원이 아니신가요?
+            </p>
+            <Link
+              to="/signup"
+              className="btn bg-brown-200 text-brown-900 hover:bg-brown-300/[0.8] w-full"
+              type="submit"
+            >
+              회원가입
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/member/Signup.tsx
+++ src/pages/member/Signup.tsx
@@ -1,5 +1,70 @@
 const Signup = () => {
-  return <>Signup</>;
+  return (
+    <div
+      className="flex items-center justify-center mx-auto text-center"
+      style={{ minHeight: `calc(100vh - 64px - 4rem - 81px)` }}
+    >
+      <div className="flex flex-col items-center max-w-md w-full">
+        <h1 className="text-2xl text-brown-900">회원가입</h1>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text text-[16px]">이메일</span>
+          </label>
+          <input
+            type="email"
+            placeholder="이메일을 입력해 주세요."
+            className="input w-full placeholder:text-sm"
+          />
+          <label className="label pb-0">
+            <span className="label-text-alt">Bottom Left label</span>
+          </label>
+
+          <label className="label">
+            <span className="label-text text-[16px]">비밀번호</span>
+          </label>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해 주세요."
+            className="input w-full placeholder:text-sm"
+          />
+          <label className="label pb-0">
+            <span className="label-text-alt">Bottom Left label</span>
+          </label>
+
+          <label className="label">
+            <span className="label-text text-[16px]">비밀번호 확인</span>
+          </label>
+          <input
+            type="password"
+            placeholder="비밀번호를 한 번 더 입력해 주세요."
+            className="input w-full placeholder:text-sm"
+          />
+          <label className="label pb-0">
+            <span className="label-text-alt">Bottom Left label</span>
+          </label>
+
+          <label className="label">
+            <span className="label-text text-[16px]">닉네임</span>
+          </label>
+          <input
+            type="password"
+            placeholder="닉네임을 입력해 주세요."
+            className="input w-full placeholder:text-sm"
+          />
+          <label className="label pb-0">
+            <span className="label-text-alt">Bottom Left label</span>
+          </label>
+
+          <button
+            className="btn bg-brown-500 text-white hover:bg-brown-600 mt-6"
+            type="submit"
+          >
+            가입하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
