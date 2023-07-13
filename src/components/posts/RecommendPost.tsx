15d4674bdfc66a8fa41bdfea40152ea638965477
Jiyeon Park <wldusys34@gmail.com>
2023-07-13 23:52:36 +0900

    Design: 추천 컴포넌트 반응형 폰트사이즈 수정, 추천 페이지 limit 수정
    - 제목: md:text-base text-sm 닉네임/날짜: md:text-sm text-xs
    - 추천 페이지 limit 개수 8 -> 6 수정

--- src/components/posts/RecommendPost.tsx
+++ src/components/posts/RecommendPost.tsx
@@ -10,8 +10,10 @@ const RecommendPost = ({ limit }: { limit: number }): JSX.Element => {
           key={index}
           className="flex items-center justify-between bg-beige w-full min-h-[64px] p-4 border-t border-brown-200 hover:bg-brown-200/[0.4] transition-all"
         >
-          <p className="text-brown-900 text-md">{recommend.title}</p>
-          <div className="text-brown-400 md:text-sm sm:text-xs">
+          <p className="text-brown-900 md:text-base text-sm">
+            {recommend.title}
+          </p>
+          <div className="text-brown-400 md:text-sm text-xs">
             <span className="px-4">{recommend.nickname}</span>
             <span>2023-07-09</span>
           </div>
