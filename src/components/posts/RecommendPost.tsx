86df7251c676972bd7413a0a2ae21a42c480ccf4
Jiyeon Park <wldusys34@gmail.com>
2023-07-20 04:52:12 +0900

    Feat: Firestore에서 추천글 데이터 불러오기(추천글 컴포넌트 수정)
    - RecommendPost 컴포넌트 샘플데이터에서 Firestore에 저장된 추천글 데이터로 코드 수정
    - Firestore에서 불러온 추천글 데이터 화면에 출력

--- src/components/posts/RecommendPost.tsx
+++ src/components/posts/RecommendPost.tsx
@@ -1,21 +1,44 @@
 import { Link } from "react-router-dom";
-import { recommendData } from "../../database/sampleData";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../database/initialize";
+import { useEffect, useState } from "react";
+import { Timestamp } from "firebase/firestore";
+
+type RecommendData = {
+  id: string;
+  nickname: string;
+  postedDate: Timestamp;
+  title: string;
+};
 
 const RecommendPost = ({ limit }: { limit: number }): JSX.Element => {
+  const [recommendDatas, setRecommendDatas] = useState<RecommendData[]>([]);
+
+  const getRecommendations = async () => {
+    const dbRecommendations = await getDocs(collection(db, "recommendations"));
+    const dataArr = dbRecommendations.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    })) as RecommendData[];
+    setRecommendDatas(dataArr);
+  };
+
+  useEffect(() => {
+    getRecommendations();
+  }, []);
+
   return (
     <div className="flex flex-col">
-      {recommendData.slice(0, limit).map((recommend, index) => (
+      {recommendDatas.slice(0, limit).map((data) => (
         <Link
-          to={`/recommend/${recommend.id}`}
-          key={index}
+          to={`/recommend/${data.id}`}
+          key={data.id}
           className="flex items-center justify-between bg-beige w-full min-h-[64px] p-4 border-t border-brown-200 hover:bg-brown-200/[0.4] transition-all"
         >
-          <p className="text-brown-900 md:text-base text-sm">
-            {recommend.title}
-          </p>
-          <div className="text-brown-400 md:text-sm text-xs">
-            <span className="px-4">{recommend.nickname}</span>
-            <span>2023-07-09</span>
+          <p className="text-brown-900 md:text-base text-sm">{data.title}</p>
+          <div className="flex items-center text-brown-400 md:text-sm text-xs">
+            <span className="px-4">{data.nickname}</span>
+            <span>{data.postedDate.toDate().toLocaleString()}</span>
           </div>
         </Link>
       ))}
