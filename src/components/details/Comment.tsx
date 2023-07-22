7d47411ea6471b200d2f307f521091a19f1c4fe8
Jiyeon Park <wldusys34@gmail.com>
2023-07-22 18:47:52 +0900

    Add: 상세페이지 댓글 컴포넌트 레이아웃
    - Comment.tsx 퍼블리싱

--- src/components/details/Comment.tsx
+++ src/components/details/Comment.tsx
@@ -1,8 +1,21 @@
 const Comment = () => {
   return (
-    <>
-      <div>댓글</div>
-    </>
+    <div>
+      <h4 className="mb-4">댓글</h4>
+      <div className="p-4 min-h-[100px] bg-beige border-t border-brown-200">
+        <div className="flex justify-between">
+          <div className="flex items-center">
+            <span className="mr-4 text-brown-500">닉네임</span>
+            <span className="text-brown-500 text-sm">날짜</span>
+          </div>
+          <div className="flex">
+            <button className="text-sm text-red">삭제하기</button>
+            <button className="text-sm ml-4">수정하기</button>
+          </div>
+        </div>
+        <div className="py-4 text-brown-600">댓글 내용</div>
+      </div>
+    </div>
   );
 };
 
