2848f00616609e2ed6b8583ef87035807bf835bf
Jiyeon Park <wldusys34@gmail.com>
2023-07-24 05:42:10 +0900

    Feat: 리뷰글 데이터 불러오기 (리뷰글 컴포넌트 수정)
    - ReviewPost 컴포넌트 Firestore에 저장된 리뷰글 데이터로 코드 수정
    - Firestore에서 불러온 리뷰 데이터 화면에 출력
    - 제품 이미지 없는 경우 고려해서 카드 레이아웃 수정

--- src/components/posts/ReviewPost.tsx
+++ src/components/posts/ReviewPost.tsx
@@ -1,42 +1,79 @@
 import { Link } from "react-router-dom";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { db } from "../../database/initialize";
+import { useEffect, useState } from "react";
 import Rating from "../common/Rating";
-import { reviewData } from "../../database/sampleData";
+
+type reviewData = {
+  id: string;
+  nickname: string;
+  brandName: string;
+  productName: string;
+  rating: number;
+  title: string;
+  content: string;
+  imageUrl?: string;
+};
 
 const ReviewPost = ({ limit }: { limit: number }): JSX.Element => {
+  const [reviewDatas, setReviewDatas] = useState<reviewData[]>([]);
+
+  const getreviews = async () => {
+    try {
+      const dbreviews = collection(db, "reviews");
+      const result = await getDocs(
+        query(dbreviews, orderBy("postedDate", "desc"))
+      );
+      const dataArr = result.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })) as reviewData[];
+      setReviewDatas(dataArr);
+    } catch (error) {
+      console.error("리스트를 불러오는 중 오류 발생:", error);
+    }
+  };
+
+  useEffect(() => {
+    getreviews();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-      {reviewData.slice(0, limit).map((review, index) => (
+      {reviewDatas.slice(0, limit).map((review, index) => (
         <Link
           to={`/review/${review.id}`}
           key={index}
-          className="card card-side bg-beige p-4 hover:drop-shadow-md transition-all outline-none"
+          className="min-h-[204px] card bg-beige p-4 hover:drop-shadow-md transition-all outline-none"
         >
-          <div>
-            <figure className="rounded md:shrink-0 mb-2">
-              <img
-                className="rounded w-[120px] h-[140px]"
-                src={review.imageUrl}
-                alt="이미지"
-              />
-            </figure>
+          <div className="flex flex-auto">
+            {review.imageUrl && (
+              <figure className="rounded md:shrink-0 mb-2 mr-3.5 ">
+                <img
+                  className="rounded w-[120px] h-[140px]"
+                  src={review.imageUrl}
+                  alt="이미지"
+                />
+              </figure>
+            )}
+            <div className="card-body p-0 gap-0">
+              <span className="text-sm text-brown-300">{review.brandName}</span>
+              <h2 className="card-title py-1 text-brown-900">
+                {review.productName}
+              </h2>
+              <p className="text-sm text-brown-500 mt-1">{review.title}</p>
+            </div>
+          </div>
+          <div className="flex justify-between items-end">
             <Rating
               rating={review.rating}
               readOnly={true}
               ratingTextVisible={false}
               setRating={() => void {}}
             />
-          </div>
-          <div className="card-body p-0 pl-3.5 gap-0">
-            <span className="text-sm text-brown-300">{review.brandName}</span>
-            <h2 className="card-title py-1 text-brown-900">
-              {review.productName}
-            </h2>
-            <p className="text-sm text-brown-500">{review.title}</p>
-            <div className="flex flex-col items-end mt-2">
-              <span className="text-right text-sm text-brown-400 my-1">
-                {review.nickname}
-              </span>
-            </div>
+            <span className="text-right text-sm text-brown-400">
+              {review.nickname}
+            </span>
           </div>
         </Link>
       ))}
