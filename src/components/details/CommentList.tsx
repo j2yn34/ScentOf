f08917d5869cb388a5f4a769879457b4c6bc3dcc
Jiyeon Park <wldusys34@gmail.com>
2023-07-24 12:45:32 +0900

    Feat: 댓글 수정 기능 구현
    - 댓글 삭제 버튼 클릭 시 confirm창 확인 후 삭제
    - 수정 버튼 클릭 시 수정/삭제 버튼 없어지고 취소/저장 버튼 구현

--- src/components/details/CommentList.tsx
+++ src/components/details/CommentList.tsx
@@ -7,6 +7,7 @@ import {
   onSnapshot,
   orderBy,
   query,
+  updateDoc,
   where,
 } from "firebase/firestore";
 import { db, auth } from "../../database/initialize";
@@ -27,6 +28,12 @@ interface CommentListProps {
 
 const CommentList: React.FC<CommentListProps> = ({ postId }) => {
   const [commentsDatas, setCommentsDatas] = useState<CommentData[]>([]);
+  const [editing, setEditing] = useState(false);
+  const [newComment, setNewComment] = useState("");
+  const [editingComment, setEditingComment] = useState<CommentData | null>(
+    null
+  );
+
   const currentUser = auth.currentUser;
 
   const getComments = async () => {
@@ -63,6 +70,32 @@ const CommentList: React.FC<CommentListProps> = ({ postId }) => {
     }
   };
 
+  const toggleEditing = () => setEditing((prev) => !prev);
+
+  const onEditClick = (comment: CommentData) => () => {
+    setNewComment(comment.content);
+    setEditingComment(comment);
+    toggleEditing();
+  };
+
+  const onSaveClick = async () => {
+    if (editingComment) {
+      try {
+        await updateDoc(doc(db, "comments", editingComment.id), {
+          content: newComment,
+        });
+        toggleEditing();
+      } catch (error) {
+        console.error("댓글 수정 중 오류 발생: ", error);
+      }
+    }
+  };
+
+  const onCancelClick = () => {
+    setEditingComment(null);
+    toggleEditing();
+  };
+
   return (
     <>
       {commentsDatas.map((comment) => (
@@ -70,7 +103,7 @@ const CommentList: React.FC<CommentListProps> = ({ postId }) => {
           key={comment.id}
           className="px-4 py-3 min-h-[100px] bg-beige border-t border-brown-200"
         >
-          <div className="mb-4">
+          <div className="mb-3">
             <div className="flex justify-between">
               <div className="flex items-center">
                 <span className="mr-4 text-brown-500">{comment.nickname}</span>
@@ -78,19 +111,50 @@ const CommentList: React.FC<CommentListProps> = ({ postId }) => {
                   <CustomDateTime timestamp={comment.createdDate.toDate()} />
                 </div>
               </div>
-              {currentUser && currentUser.uid === comment.userId && (
-                <div className="flex">
-                  <button
-                    onClick={onDeleteClick(comment)}
-                    className="text-sm text-red"
-                  >
-                    삭제하기
-                  </button>
-                  <button className="text-sm ml-4">수정하기</button>
-                </div>
-              )}
+              {currentUser &&
+                currentUser.uid === comment.userId &&
+                !editing && (
+                  <div className="flex">
+                    <button
+                      onClick={onDeleteClick(comment)}
+                      className="text-sm text-red"
+                    >
+                      삭제
+                    </button>
+                    <button
+                      onClick={onEditClick(comment)}
+                      className="text-sm ml-4"
+                    >
+                      수정
+                    </button>
+                  </div>
+                )}
             </div>
-            <div className="pt-3 text-brown-600">{comment.content}</div>
+            {editing && editingComment?.id === comment.id ? (
+              <div className="flex">
+                <textarea
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                  className="my-2 p-2 w-full bg-beige placeholder:text-brown-300 placeholder:italic"
+                />
+                <div className="flex items-center">
+                  <button
+                    onClick={onCancelClick}
+                    className="text-sm w-[36px] h-[30px] ml-2"
+                  >
+                    취소
+                  </button>
+                  <button
+                    onClick={onSaveClick}
+                    className="text-sm text-green min-w-[36px] h-[30px] ml-3"
+                  >
+                    저장
+                  </button>
+                </div>
+              </div>
+            ) : (
+              <div className="pt-3 text-brown-600">{comment.content}</div>
+            )}
           </div>
         </div>
       ))}
