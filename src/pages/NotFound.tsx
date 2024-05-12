75d2f3207bb70c573a1927277de827a2338975e7
Jiyeon Park <wldusys34@gmail.com>
2024-05-13 03:30:37 +0900

    Design: NotFound 페이지 디자인 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,8 +2,9 @@ const NotFound = () => {
   return (
     <div className="flex justify-center items-center mt-48">
       <div className="flex flex-col justify-center items-center">
-        <h2>페이지를 찾을 수 없습니다</h2>
-        <button className="btn mt-8">메인페이지로 이동</button>
+        <h1 className="text-4xl font-bold p-2">404</h1>
+        <span className="text-xl">페이지를 찾을 수 없습니다</span>
+        <button className="btn mt-12 primary-btn">메인페이지로 이동</button>
       </div>
     </div>
   );
