6bd05a77f771d9bafad486a1313a874e424bfa10
Jiyeon Park <wldusys34@gmail.com>
2024-05-13 03:37:48 +0900

    Feat: NotFound페이지에서 메인페이지로 이동 버튼 구현

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="flex justify-center items-center mt-48">
       <div className="flex flex-col justify-center items-center">
         <h1 className="text-4xl font-bold p-2">404</h1>
         <span className="text-xl">페이지를 찾을 수 없습니다</span>
-        <button className="btn mt-12 primary-btn">메인페이지로 이동</button>
+        <Link className="btn mt-12 primary-btn" to={"/"}>
+          메인페이지로 이동
+        </Link>
       </div>
     </div>
   );
