3045594b795b18923938639f15785ca92d808ed7
Jiyeon Park <wldusys34@gmail.com>
2024-05-29 23:37:43 +0900

    Feat: 추천검색어 클릭 시 검색 기능 구현

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -11,6 +11,17 @@ const SearchPage = () => {
   const recommendCount = useRecoilValue(recommendCountState);
   let totalCount = reviewCount + recommendCount;
 
+  const exampleSearchTerms = [
+    { name: "lanvin", term: "랑방" },
+    { name: "dior", term: "디올" },
+    { name: "summer", term: "여름" },
+  ];
+
+  const clickExamples = (exTerm: string) => {
+    setSubmittedSearchTerm(exTerm);
+    setSearchTerm(exTerm);
+  };
+
   const checkSearchValue = async (searchTerm: string) => {
     const trimmedSearchTerm = searchTerm.trim();
     if (trimmedSearchTerm === "") {
@@ -29,18 +40,34 @@ const SearchPage = () => {
 
   return (
     <div className="pt-14 px-4">
-      <form onSubmit={onSubmit} className="flex justify-center">
-        <input
-          type="text"
-          className="border-b border-brown p-2 mr-4 w-[280px] focus:outline-none placeholder:text-brown-300 placeholder:italic"
-          placeholder="브랜드명, 제품명을 검색해 보세요."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <button type="submit" className="btn primary-btn px-6">
-          검색
-        </button>
-      </form>
+      <div className="w-100">
+        <form onSubmit={onSubmit} className="flex justify-center">
+          <input
+            type="text"
+            className="border-b border-brown p-2 mr-4 w-[280px] focus:outline-none placeholder:text-brown-300 placeholder:italic"
+            placeholder="브랜드명, 제품명을 검색해 보세요."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type="submit" className="btn primary-btn px-6">
+            검색
+          </button>
+        </form>
+        <div className="flex items-center justify-start pt-4">
+          <span className="text-brown-500">추천검색어</span>
+          <div>
+            {exampleSearchTerms.map((searchTerm) => (
+              <button
+                key={searchTerm.name}
+                className="badge badge-lg py-4 ml-2.5 text-brown-400 hover:hover:bg-beige"
+                onClick={() => clickExamples(searchTerm.term)}
+              >
+                {searchTerm.term}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
       {submittedSearchTerm && (
         <>
           <p className="text-sm pt-4 text-brown-500">
