a5a98cba8a630e11516f4652df061a6a1aa694f9
Jiyeon Park <wldusys34@gmail.com>
2024-05-30 23:45:28 +0900

    Feat: 추천글 제목 말줄임표 구현

--- src/components/posts/RecommendCard.tsx
+++ src/components/posts/RecommendCard.tsx
@@ -9,7 +9,9 @@ const RecommendCard = ({ data }: { data: PostData }): JSX.Element => {
       key={data.id}
       className="flex items-center justify-between bg-beige w-full min-h-[64px] p-4 border-t border-brown-200 hover:bg-brown-200/[0.4] transition-all"
     >
-      <p className="text-brown-900 md:text-base text-sm">{data.title}</p>
+      <p className="text-brown-900 md:text-base text-sm truncate">
+        {data.title}
+      </p>
       <div className="flex items-center text-brown-400 md:text-sm text-xs">
         <span className="md:mx-4 mx-2.5 text-center md:w-[90px] w-[60px]">
           {data.nickname}
